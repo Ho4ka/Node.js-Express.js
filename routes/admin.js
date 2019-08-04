d66898760ed1882e87d75ce09a3f0c7f188a9a8f
Ho4ka <m.domaretsky@gmail.com>
2019-08-04 16:18:24 +0300

    Helper fucntion for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
 const path = require('path');
 // /admin/add-product => GET
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
