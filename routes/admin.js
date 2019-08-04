578f255886f6812592786b2c270d7a0e083b9d57
Ho4ka <m.domaretsky@gmail.com>
2019-08-04 15:43:54 +0300

    Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const path = require('path');
 // /admin/add-product => GET
 
 router.get('/add-product', (req, res, next) => {
-    res.send("<form action='/admin/add-product' method='post'> <input type='text' name='title'><button type='submit'>Add product</button></form>");
+    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
