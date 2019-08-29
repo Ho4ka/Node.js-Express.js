c1208814871b0e285cb0aee9393b3f4d22b25f3f
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-08-29 13:58:57 +0300

    Inserting data & creaiting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(() => {
-      res.redirect('/');
-  }).catch((error) => {
-      console.log(error);
-  });
+  Product.create({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+  })
+      .then(result => {
+          console.log('Created Product');
+      })
+      .catch(error => {
+          console.log(error);
+      });
 };
 
 exports.getEditProduct = (req, res, next) => {
