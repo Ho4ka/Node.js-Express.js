588c9d283740f8d5a2f66b1d10384b83858a1021
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-08-31 21:50:05 +0300

    Installing mongoDb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '763315Maks', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Maks:763315Maks@cluster0-llm4m.mongodb.net/test?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected');
+            callback(client );
+        })
+        .catch(error => console.log(error));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
