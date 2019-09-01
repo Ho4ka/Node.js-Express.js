ed5d4ccc971f83ccb2e23cfe4a168f0224324166
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-09-01 22:43:02 +0300

    Connecting to mongoDB Compass

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://Maks:763315Maks@cluster0-llm4m.mongodb.net/shop ?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://Maks:763315Maks@cluster0-llm4m.mongodb.net/test ?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected');
             _db = client.db();
