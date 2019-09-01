ac81b0ed63e9462521c5b977f2275d58d6c365c5
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-09-01 14:04:23 +0300

    Creating mongoDB connection

--- util/database.js
+++ util/database.js
@@ -1,13 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://Maks:763315Maks@cluster0-llm4m.mongodb.net/test?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://Maks:763315Maks@cluster0-llm4m.mongodb.net/shop ?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected');
-            callback(client);
+            _db = client.db();
+            callback();
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+          console.log(error);
+          throw error
+        });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+      return _db;
+    }
+
+    throw 'No database found!'
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
