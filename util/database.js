e49818f2f2fa6d73f154d3a227996335575d6a4d
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-09-01 13:51:14 +0300

    Creating mongoDB connection

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ const mongoConnect = (callback) => {
     MongoClient.connect('mongodb+srv://Maks:763315Maks@cluster0-llm4m.mongodb.net/test?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected');
-            callback(client );
+            callback(client);
         })
         .catch(error => console.log(error));
 };
