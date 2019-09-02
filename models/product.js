38976586cb557574e79a1d0bb77b80c944d274e2
maksym.domaretskyi <m.domaretsky@gmail.com>
2019-09-02 22:21:48 +0300

    Fetching single item with mongoDB

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
+
 
 class Product {
     constructor(title, price, description, imageUrl) {
@@ -20,8 +22,22 @@ class Product {
             });
     }
 
-    static fetchAll() {
+   static findById(prodId) {
         const db = getDb();
+        return db.collection('products')
+            .find({ _id: new mongodb.ObjectId(prodId) })
+            .next()
+            .then(product => {
+                console.log(product);
+                return product;
+            })
+            .catch(error => {
+                console.log(error);
+        })
+    }
+
+    static fetchAll() {
+       const db = getDb();
        return db
            .collection('products')
            .find()
